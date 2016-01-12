3dd85e8fc95d3c2a3416d1c7f2fbd5f39bc01650
Alfonsodan <danalfons@yahoo.com>
2016-01-12 17:02:31 -0500

    added layout and the buttons now work like they should.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,86 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * Global Variables go below me!
+     */
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method describes what happens when the 3 point button is clicked. !!TEAM A!!
+     */
+    public void threePointButton(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This method describes what happens with the 2 point button is clicked.  !!TEAM A!!
+     */
+    public void twoPointButton(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This method describes what happens when the free throw button is clicked. !!TEAM A!!
+     */
+    public void freeThrowButton(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.  !!TEAM A!!
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method describes what happens when the 3 point button is clicked. !!TEAM B!!
+     */
+    public void threePointButtonB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method describes what happens with the 2 point button is clicked.  !!TEAM B!!
+     */
+    public void twoPointButtonB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method describes what happens when the free throw button is clicked. !!TEAM B!!
+     */
+    public void freeThrowButtonB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.  !!TEAM B!!
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
